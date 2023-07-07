bd56d3d2ee81e72dd4196329d4362df22ee6164d
Anindya Ghosh <anindya128@yahoo.com>
2023-07-07 17:31:37 +0100

    Converting downsampling to Tonic format to add to Tonic

---
+++ eventdownsampleTonic.py
@@ -0,0 +1,282 @@
+from dataclasses import dataclass
+from typing import Callable, List, Optional, Tuple, Union
+import numpy as np
+import os
+
+root = os.path.dirname(os.path.abspath(__file__))
+
+class eventDownsample:
+    """Performs high fidelity event based spatio-temporal down-sampling.
+    Arguments:
+        sensor_size (tuple): a 3-tuple of x,y,p for sensor_size
+        target_size (tuple): a 2-tuple of x,y denoting new down-sampled size for events to be re-scaled to (new_width, new_height).
+        noise_threshold (int): threshold value for number of events of each polarity in each mega-pixel allowed
+                                 before a spike is emitted in the down-sampled pixel.
+        timesteps_per_frame (int)
+        version (string): 'naive' for naive event down-sampling -> tonic.transforms.Downsample.
+                          'integ' for differentiator based event down-sampling.
+                          'diff' for integrator based event down-sampling.
+                          'global' -> naive event-based down-sampling with spatial_factor=1.0 and noise_threshold = 0.
+    Example:
+        >>> from tonic.transforms import eventDownsample
+        >>> transform1 = eventDownsample(time_factor=0.001) # change us to ms
+        >>> transform2 = eventDownsample(target_size=(20, 20)), version='v2', 
+                                         noise_threshold=10) # reduce x/y plane to 20x20 pixels, 
+                                         'v2' down-sampling and noise_threshold set to 10 events of each polarity.
+    """
+
+    sensor_size: Tuple[int, int, int] = (640, 480, 2)
+    target_size: Tuple[int, int] = (20, 20)
+    timesteps_per_frame: int = 33
+    _noise_thr: int = 0
+    
+    def __init__(self, events, sensor_size, target_size, timesteps_per_frame, version, _noise_thr=0):
+        self.filename = events
+        self.original_resolution = sensor_size[:-1]
+        self.new_resolution = target_size
+        self.version = version
+        self.timesteps_per_frame = timesteps_per_frame
+        self._noise_thr = _noise_thr
+        
+    def path(self):
+        outDir = 'polarity_add33' if self.version == 'v1' else 'polarity_add'
+        # XXX: Fine for now but need to change for tonic
+        dirFolder = 'dvx_dataset'
+        self.Path = os.path.join(root, dirFolder, outDir, self.filename)
+    
+    def resolution_check(self):
+        if self.version == 'global':
+            self.new_resolution = self.original_resolution
+            self.noise_thr = 0
+        self.scale = self.original_resolution / self.new_resolution
+        
+    def adaptive_threshold(self):
+        # noise_thr[t] = noise_thr[t-1] * np.exp(-1 / THRESHOLD_TAU)
+        # TODO: Implement adaptive threshold -- Tau ~ 10 ms
+        # Without is alright for simple stimulus
+        noise_thr = None
+        
+    def noise_threshold(self):
+        if self.version == 'naive':
+            self.noise_thr = 0
+        elif self.version == 'v1':
+            reshape_scale = 1 # np.sqrt(self.scale[0] * self.scale[1])
+            self.noise_thr = np.ceil(reshape_scale / 4)
+        elif self.version == 'v2':
+            self.noise_thr = self._noise_thr # self.scale[0] * self.scale[1] * (100 / 27.5) * 0.225 * 0.05
+            
+        # Spatially segregated amacrine cells for ON and OFF pathways
+        # Zhang C, Yu WQ, Hoshino A, Huang J, Rieke F, Reh TA, Wong ROL. 
+        # Development of ON and OFF cholinergic amacrine cells in the human fetal retina. J Comp Neurol. 
+        # 2019 Jan 1;527(1):174-186. doi: 10.1002/cne.24405. Epub 2018 Feb 25. PMID: 29405294; PMCID: PMC6078822.
+    
+    def frame_build(self, new_split):
+        
+        if new_split.shape[1] > 0:
+            frame_input_image = np.histogram2d(np.abs(new_split[0,:]), np.abs(new_split[1,:]),
+                                                [np.arange(self.new_resolution[1] + 1), np.arange(self.new_resolution[0] + 1)])[0]
+        else:
+            frame_input_image = np.zeros((self.new_resolution[1], self.new_resolution[0]))
+        return frame_input_image
+    
+# ----------------------------------------------------------------------------
+# V1  
+    
+    def frame_input_spike(self, x, y):
+        
+        spk_mat = np.zeros((self.new_resolution[0] * self.new_resolution[1], 1))
+        
+        new_spike_id = np.multiply(x, self.new_resolution[0]) + y
+        spk_mat[new_spike_id] += 1
+            
+        return spk_mat
+
+    def timestep_spikes(self, frames):
+        
+        num_frames = frames[-1][0] + 1
+        timestep_inputs = np.zeros((self.new_resolution[0] * self.new_resolution[1], 1, num_frames), np.float32)
+        timestep_adj = np.zeros((self.new_resolution[0] * self.new_resolution[1], 1, num_frames-1), np.float32)
+        for i, f in frames:
+            timestep_inputs[:,:,i] += f
+            
+        for a in np.arange(num_frames-1):
+            timestep_adj[:,:,a] += np.subtract(timestep_inputs[:,:,a+1], timestep_inputs[:,:,a])
+            
+        timestep_adj[timestep_adj[:,:] < 0] = 0
+            
+        return timestep_adj
+
+    def input_spikes(self, timestep_inputs):
+        
+        neuron_spike_times = [[] for x in np.arange(self.new_resolution[0] * self.new_resolution[1])]
+        
+        for i in np.arange(timestep_inputs.shape[2]):
+            ind = np.where(timestep_inputs[:,:,i] > 0)[0]
+            for elem in ind:
+                neuron_spike_times[elem].append(i)
+            
+        return neuron_spike_times
+
+# ----------------------------------------------------------------------------
+
+    def naive(self):
+        
+        # Take histogram so as to assemble frame
+        frame_input_image_pos = self.frame_build(self.new_pos)
+        frame_input_image_neg = -self.frame_build(self.new_neg)
+        
+        # lst.append((time, np.max(frame_input_image)))
+        
+        # Noise filtering
+        self.x_pos, self.y_pos = np.where(frame_input_image_pos[:,:] > self.noise_thr)
+        self.x_neg, self.y_neg = np.where(frame_input_image_neg[:,:] < -self.noise_thr)
+        
+        new_spike_id_pos = np.multiply(self.x_pos, self.new_resolution[0]) + self.y_pos
+        new_spike_id_neg = np.multiply(self.x_neg, self.new_resolution[0]) + self.y_neg
+        
+        for elem in new_spike_id_pos:
+            self.neuron_spike_times_pos[elem].append(self.time)
+            
+        for elem in new_spike_id_neg:
+            self.neuron_spike_times_neg[elem].append(self.time)
+            
+    def v1(self):
+        
+        # Take histogram so as to assemble frame
+        frame_input_image_pos = self.frame_build(self.new_pos)
+        frame_input_image_neg = self.frame_build(self.new_neg)
+        
+        frame_input_image = np.subtract(frame_input_image_pos, frame_input_image_neg)
+        
+        # lst.append((time, np.max(frame_input_image)))
+        
+        # Noise filtering
+        self.x_pos, self.y_pos = np.where(frame_input_image[:,:] > self.noise_thr)
+        self.x_neg, self.y_neg = np.where(frame_input_image[:,:] < -self.noise_thr)
+        
+        pos_add = self.frame_input_spike(self.x_pos, self.y_pos)
+        neg_add = self.frame_input_spike(self.x_neg, self.y_neg)
+        
+        self.frames_pos.append((self.frame, pos_add))
+        self.frames_neg.append((self.frame, neg_add))
+        
+    def v1_finish(self):
+        timestep_pos = self.timestep_spikes(self.frames_pos)
+        timestep_neg = self.timestep_spikes(self.frames_neg)
+        
+        neuron_spike_times_pos = self.input_spikes(timestep_pos)
+        neuron_spike_times_neg = self.input_spikes(timestep_neg)
+        
+        sim_time = int(self.time / self.timesteps_per_frame)
+        
+        return sim_time, neuron_spike_times_pos, neuron_spike_times_neg
+    
+    def v2(self):
+        frame_input_image_pos = self.frame_build(self.new_pos)
+        frame_input_image_neg = self.frame_build(self.new_neg)
+        
+        frame_input_image = np.subtract(frame_input_image_pos, frame_input_image_neg)
+        
+        # lst.append((time, np.max(frame_input_image)))
+        
+        # Graded addition of spikes
+        self.frame_spike += frame_input_image
+        
+        for i in np.arange(self.new_resolution[1]):
+            for j in np.arange(self.new_resolution[0]):
+                pix = i * self.new_resolution[0] + j
+                if self.frame_spike[i, j] > self.noise_thr:
+                    self.neuron_spike_times_pos[pix].append(self.time)
+                    self.frame_spike[i, j] = 0
+                elif self.frame_spike[i, j] < -self.noise_thr:
+                    self.neuron_spike_times_neg[pix].append(self.time)
+                    self.frame_spike[i, j] = 0
+                else:
+                    pass
+                
+    def spike_global(self):
+        
+        # Take histogram so as to assemble frame
+        frame_input_image_pos = self.frame_build(self.new_pos)
+        frame_input_image_neg = self.frame_build(self.new_neg)
+        
+        # Eliminate temporal downsampling jitter
+        frame_input_image = np.subtract(frame_input_image_pos, frame_input_image_neg)
+        
+        self.x_pos, self.y_pos = np.where(frame_input_image[:,:] > 0)
+        self.x_neg, self.y_neg = np.where(frame_input_image[:,:] < 0)
+        
+        pos_spks = np.ravel_multi_index([self.x_pos, self.y_pos], (self.new_resolution[1], self.new_resolution[0]))
+        neg_spks = np.ravel_multi_index([self.x_neg, self.y_neg], (self.new_resolution[1], self.new_resolution[0]))
+        
+        [self.neuron_spike_times_pos[elem].append(self.time) for elem in pos_spks]
+        [self.neuron_spike_times_neg[elem].append(self.time) for elem in neg_spks]
+            
+    def read_input_spikes(self):
+        self.path()
+        self.resolution_check()
+        self.noise_threshold()
+        
+        self.neuron_spike_times_pos = [[] for x in np.arange(self.new_resolution[0] * self.new_resolution[1])]
+        self.neuron_spike_times_neg = [[] for x in np.arange(self.new_resolution[0] * self.new_resolution[1])]
+        
+        self.frames_pos = []
+        self.frames_neg = []
+        
+        self.frame_spike = np.zeros((self.new_resolution[1], self.new_resolution[0]), np.float32)
+        
+        with open(self.Path, "r") as input_spike_file:
+            # Read input spike file
+            for line in input_spike_file:
+                
+                # Split lines into time and keys
+                time_string, keys_string = line.split(";")
+                self.time = int(time_string)
+                self.frame = self.time // self.timesteps_per_frame
+                
+                # Load spikes into numpy array
+                frame_input_spikes = np.asarray(keys_string.split(","), dtype=str)
+                neg_track = [i for i, elem in enumerate(frame_input_spikes) if '-' in elem]
+                frame_input_spikes = np.abs(frame_input_spikes.astype(np.int32))
+        
+                # Split into X and Y
+                frame_input_x = np.floor_divide(frame_input_spikes, self.original_resolution[0])
+                frame_input_y = np.remainder(frame_input_spikes, self.original_resolution[0])
+                
+                if self.version == 'global':
+                    new_ = np.vstack((frame_input_x, frame_input_y))
+                else:
+                    # X and Y now correspond to rows and columns respectively
+                    new_x = np.floor(frame_input_x / self.scale[1])
+                    new_y = np.floor(frame_input_y / self.scale[0])
+                    
+                    new_ = np.vstack((new_x, new_y))
+                    new_ = new_.astype(int)
+                
+                self.new_neg = np.take(new_, neg_track, axis=1)
+                
+                self.new_neg[0, :] *= -1
+                
+                pos_track = np.setdiff1d(np.arange(new_.shape[1]), neg_track)
+                self.new_pos = np.take(new_, pos_track, axis=1)
+                
+                if self.version == 'naive':
+                    self.naive()
+                elif self.version == 'v1':
+                    self.v1()
+                elif self.version == 'v2':
+                    self.v2()
+                elif self.version == 'global':
+                    self.spike_global()
+                    
+            if self.version == 'v1':
+                sim_time, neuron_spike_times_pos, neuron_spike_times_neg = self.v1_finish()
+                self.time = sim_time
+                self.neuron_spike_times_pos = neuron_spike_times_pos
+                self.neuron_spike_times_neg = neuron_spike_times_neg
+                    
+        return self.time, self.neuron_spike_times_pos, self.neuron_spike_times_neg
+
+# new_resolution = np.array([20, 20])
+# x = extractSourceSpikes('do-b_f_0.spikes', new_resolution, 'v1')
+# time, neuron_spike_times_pos, neuron_spike_times_neg = x.read_input_spikes()
---
+++ functional_downsampling.py
@@ -0,0 +1,103 @@
+import time
+import numpy as np
+
+events = np.load('dvs_struct_array.npy')
+
+def naive_downsample(events: np.ndarray, sensor_size: tuple, target_size: tuple):
+    """Downsample the classic "naive" Tonic way. Multiply x/y values by a spatial_factor 
+    obtained by dividing sensor size by the target size.
+    Parameters:
+        events (ndarray): ndarray of shape [num_events, num_event_channels].
+        sensor_size (tuple): a 3-tuple of x,y,p for sensor_size.
+        target_size (tuple): a 2-tuple of x,y denoting new down-sampled size for events to be
+                             re-scaled to (new_width, new_height).
+    Returns:
+        the downsampled input events.
+    """
+    
+    assert "x" and "y" in events.dtype.names
+    
+    events = events.copy()
+    
+    spatial_factor = np.asarray(target_size) / sensor_size[:-1]
+
+    events["x"] = events["x"] * spatial_factor[1]
+    events["y"] = events["y"] * spatial_factor[0]
+
+    return events
+
+def time_bin_numpy(events: np.ndarray, time_bin_interval: float):
+    """Temporally downsample the events into discrete time bins as stipulated by time_bin_intervals.
+    Parameters:
+        events (ndarray): ndarray of shape [num_events, num_event_channels].
+        time_bin_interval (float): time bin size for events e.g. every 0.5 ms: time_bin_interval = 0.5.
+    Returns:
+        the input events with rewritten timestamps.
+    """
+    
+    events = events.copy()
+    reciprocal_interval = 1 / time_bin_interval
+    events["t"] = np.round(events["t"] * reciprocal_interval, 0) / reciprocal_interval
+    
+    return events
+
+def diff_downsample(events: np.ndarray, sensor_size: tuple, target_size: tuple, noise_threshold: int = 0):
+    """Downsample using an integrate-and-fire (I-F) neuron model with a noise threshold similar to 
+    the membrane potential threshold in the I-F model. Multiply x/y values by a spatial_factor 
+    obtained by dividing sensor size by the target size.
+    Parameters:
+        events (ndarray): ndarray of shape [num_events, num_event_channels].
+        sensor_size (tuple): a 3-tuple of x,y,p for sensor_size.
+        target_size (tuple): a 2-tuple of x,y denoting new down-sampled size for events to be
+                             re-scaled to (new_width, new_height).
+    Returns:
+        the downsampled input events using the differentiator method.
+    """
+        
+    assert "x" and "y" and "t" in events.dtype.names
+    
+    events = events.copy()
+    
+    # Downsample
+    spatial_factor = np.asarray(target_size) / sensor_size[:-1]
+
+    events["x"] = events["x"] * spatial_factor[1]
+    events["y"] = events["y"] * spatial_factor[0]
+    
+    # All event times
+    event_times = np.unique(events["t"])
+    
+    # Separate by polarity
+    events_positive = events[events["p"] == 1]
+    events_negative = events[events["p"] == 0]
+    
+    # Running buffer of events in each pixel
+    frame_spike = np.zeros(target_size)
+    
+    events_new = []
+    
+    for time in event_times:
+        xy_pos = events_positive[events_positive["t"] == time][["x", "y"]]
+        xy_neg = events_negative[events_negative["t"] == time][["x", "y"]]
+        
+        frame_histogram = np.subtract(np.histogram2d(xy_pos["x"], xy_pos["y"], [range(target_size[1] + 1), range(target_size[0] + 1)])[0],
+                                      np.histogram2d(xy_neg["x"], xy_neg["y"], [range(target_size[1] + 1), range(target_size[0] + 1)])[0])
+        
+        frame_spike += frame_histogram
+        
+        coordinates_pos = np.stack(np.nonzero(np.maximum(frame_spike > noise_threshold, 0))).T
+        coordinates_neg = np.stack(np.nonzero(np.maximum(-frame_spike > noise_threshold, 0))).T
+        
+        events_new.append(np.column_stack((coordinates_pos, np.ones((coordinates_pos.shape[0],1)), time*np.ones((coordinates_pos.shape[0],1)))))
+        events_new.append(np.column_stack((coordinates_neg, np.zeros((coordinates_neg.shape[0],1)), time*np.ones((coordinates_neg.shape[0],1)))))
+        
+    events_new = np.concatenate(events_new.copy())
+    
+    return np.core.records.fromarrays(events_new.copy().T, dtype=events.dtype)
+
+# Time bin of 1 ms
+events = time_bin_numpy(events, 1.0)
+t1 = time.perf_counter()
+events_diff = diff_downsample(events, (128, 128, 2), (16, 16))
+t2 = time.perf_counter()
+print(f'{t2-t1:.2f}')
